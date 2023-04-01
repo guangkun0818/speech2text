e68de807a10957116d5ff06bcb1bc752ecbfea72
guangkun0818 <609946862@qq.com>
2023-04-01 16:22:52 +0800

    [dataset_impl] - impl tokenizer of dataset utilies, unittest ensured.

---
+++ dataset/utils.py
@@ -0,0 +1,191 @@
+# Author: guangkun0818
+# Email: 609946862@qq.com
+# Created on 2023.03.29
+""" Necessary utilities of Dataset, including Tokenizer 
+    and collate_fn impl. More to dev. 
+"""
+
+import abc
+import dataclasses
+import torch
+import sentencepiece as spm
+
+from typing import List, Dict
+from torch.nn.utils.rnn import pad_sequence
+
+
+class Tokenizer(abc.ABC):
+    """ Abstract class template for different tokenizer support """
+
+    @property
+    @abc.abstractmethod
+    def labels(self) -> List[str]:
+        """ return all tokens """
+        ...
+
+    @abc.abstractmethod
+    def encode(self, text: str) -> torch.Tensor:
+        """ Encode raw text as vector """
+        ...
+
+    @abc.abstractmethod
+    def decode(self, vector: torch.Tensor) -> str:
+        """ Decode list of tokens as raw text """
+        ...
+
+    @abc.abstractmethod
+    def encode_as_tokens(self, text: str) -> List[str]:
+        # Encode text as tokens rather than vector, used during inference
+        ...
+
+    @abc.abstractmethod
+    def decode_from_tokens(self, tokens: List[str]) -> str:
+        # Decode list of tokens as raw text, used during inference
+        ...
+
+    def _text_to_vector(self, text: List[str]) -> torch.Tensor:
+        """ Return: torch.Tensor(nums_tokens) """
+        vector = []
+        for char in text:
+            assert char in self.labels, "{} is not in vocabulary {}".format(
+                char, self.labels)
+            vector.append(self.labels.index(char))
+        vector = torch.Tensor(vector).long()
+        return vector
+
+    def _vector_to_tokens(self, vector: torch.Tensor) -> List[str]:
+        """ Return: tokens (seq_lens of vector) """
+        tokens = []
+        for idx in vector.tolist():
+            tokens.append(self.labels[idx])
+        return tokens
+
+
+@dataclasses.dataclass
+class CharTokenizerConfig:
+    """ Char based Tokenizer Config, should be configured from yaml """
+    labels: tuple = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+                     "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
+                     "y", "z", "'", " ")
+
+
+@dataclasses.dataclass
+class SubwordTokenizerConfig:
+    """ Subword based Tokenzier Config, spm_model and spm_vocab should 
+        provided in yaml. 
+    """
+    spm_model: str = None
+    spm_vocab: str = None
+
+
+class SubwordTokenizer(Tokenizer):
+    """ Subword based tokenzier impl """
+
+    def __init__(self, config: SubwordTokenizerConfig):
+        super(SubwordTokenizer, self).__init__()
+
+        assert config.spm_model is not None
+        assert config.spm_vocab is not None
+        self._spm_model = config.spm_model
+        self._spm_vocab = config.spm_vocab
+        self._labels = self._load_labels(self._spm_vocab)
+        self._sp = spm.SentencePieceProcessor()
+        self._sp.Load(self._spm_model)
+
+    @property
+    def labels(self) -> List[str]:
+        return self._labels
+
+    def _load_labels(self, vocabs) -> List[str]:
+        labels = ["<blank_id>"]
+        with open(vocabs, 'r') as vocabs_f:
+            for line in vocabs_f:
+                token = line.strip().split("\t")[0]
+                # Get rid of <s> and </s>
+                if token != "<s>" and token != "</s>":
+                    labels.append(token)
+        labels.append("<sos>")  # add <sos> token for rescore and Rnnt
+        return labels
+
+    def encode(self, text: str) -> torch.Tensor:
+        return self._text_to_vector(self._sp.EncodeAsPieces(text))
+
+    def decode(self, vector: torch.Tensor) -> str:
+        return self._sp.DecodePieces(self._vector_to_tokens(vector))
+
+    def encode_as_tokens(self, text: str) -> List[str]:
+        tokens = self._sp.EncodeAsPieces(text)
+        for char in tokens:
+            assert char in self.labels, "Out of vocabulary detected with '{}'".format(
+                char)
+        return tokens
+
+    def decode_from_tokens(self, tokens: List[str]) -> str:
+        for char in tokens:
+            assert char in self.labels, "Out of vocabulary detecte with '{}'".format(
+                char)
+        return self._sp.DecodePieces(tokens)
+
+
+class CharTokenizer(Tokenizer):
+    """ Char based tokenzier impl """
+
+    def __init__(self, config: CharTokenizerConfig) -> None:
+        super(CharTokenizer, self).__init__()
+
+        # Insert <blank_id> at front and <sos> at rear
+        self._labels = ["<blank_id>"] + config.labels + ["<sos>"]
+
+    @property
+    def labels(self):
+        return self._labels
+
+    def encode(self, text: str) -> torch.Tensor:
+        return self._text_to_vector(text)
+
+    def decode(self, vector: torch.Tensor) -> str:
+        return ''.join(self._vector_to_tokens(vector))
+
+    def encode_as_tokens(self, text: str) -> List[str]:
+        tokens = list(text)
+        for char in tokens:
+            assert char in self.labels, "Out of vocabulary detected with '{}'".format(
+                char)
+        return tokens
+
+    def decode_from_tokens(self, tokens: List[str]) -> str:
+        for char in tokens:
+            assert char in self.labels, "Out of vocabulary detects with '{}'".format(
+                char)
+        return ''.join(tokens)
+
+
+def TokenizerSetup(config) -> Tokenizer:
+    """ Interface of Tokenizer Setup """
+    if config["type"] == "char":
+        return CharTokenizer(config=CharTokenizerConfig(**config["config"]))
+    elif config["type"] == "subword":
+        return SubwordTokenizer(config=SubwordTokenizerConfig(
+            **config["config"]))
+    else:
+        raise ValueError(
+            "Only 'char' and 'subword' tokenizer supported currently.")
+
+
+def batch(batch: Dict[str, List]) -> Dict[str, torch.Tensor]:
+    """ Padding dynamic length of inputs and stack as off-to-go batch. """
+    assert "feat" in batch
+    assert "feat_length" in batch
+    assert "label" in batch
+    assert "label_length" in batch
+
+    batch["feat"] = pad_sequence(batch["feat"],
+                                 batch_first=True,
+                                 padding_value=0)
+    batch["feat_length"] = torch.Tensor(batch["feat_length"]).long()
+    batch["label"] = pad_sequence(batch["label"],
+                                  batch_first=True,
+                                  padding_value=0)
+    batch["label_length"] = torch.Tensor(batch["label_length"]).long()
+
+    return batch
---
+++ dataset/utils_test.py
@@ -0,0 +1,123 @@
+# Author: guangkun0818
+# Email: 609946862@qq.com
+# Created on 2023.04.01
+""" Unittest of Dataset Utilities """
+
+import unittest
+import glog
+import torch
+
+from parameterized import parameterized
+
+from dataset.utils import CharTokenizer, CharTokenizerConfig
+from dataset.utils import SubwordTokenizer, SubwordTokenizerConfig
+
+
+class TestCharTokenizer(unittest.TestCase):
+    """ Unittest of Char Tokenzier """
+
+    def setUp(self) -> None:
+
+        self._config = {
+            "labels": [
+                "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+                "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+                "'", " "
+            ]
+        }
+
+        self._tokenizer = CharTokenizer(config=CharTokenizerConfig(
+            **self._config))
+
+    def test_num_labels(self):
+        self.assertEqual(
+            len(self._config["labels"]) + 2, len(self._tokenizer.labels))
+        self.assertIn("<blank_id>", self._tokenizer.labels)
+        self.assertEqual("<blank_id>", self._tokenizer.labels[0])
+        self.assertIn("<sos>", self._tokenizer.labels)
+        self.assertEqual("<sos>", self._tokenizer.labels[-1])
+
+    @parameterized.expand([
+        ("season with salt and pepper and a little sugar to taste"),
+        ("struggle warfare was the condition of private ownership it was fatal"
+        ),
+        ("i don't believe ann knew any magic or she'd have worked it before"),
+        ("a black drove came up over the hill behind the wedding party")
+    ])
+    def test_encode_as_tensor(self, text):
+        # Unittest of encode and decode
+        glog.info(text)
+        labels = self._tokenizer.encode(text)
+        decoded = self._tokenizer.decode(labels)
+
+        self.assertEqual(text, decoded)
+
+    @parameterized.expand([
+        ("season with salt and pepper and a little sugar to taste"),
+        ("struggle warfare was the condition of private ownership it was fatal"
+        ),
+        ("i don't believe ann knew any magic or she'd have worked it before"),
+        ("a black drove came up over the hill behind the wedding party")
+    ])
+    def test_encoder_as_tokens(self, text):
+        # Unittest of encode_as_tokens and decode_from_tokens
+        glog.info(text)
+        tokens = self._tokenizer.encode_as_tokens(text)
+        decoded = self._tokenizer.decode_from_tokens(tokens)
+
+        self.assertEqual(text, decoded)
+
+
+class TestSubwordTokenizer(unittest.TestCase):
+    """ Unittest of Subword Tokenizer """
+
+    def setUp(self) -> None:
+        self._config = {
+            "spm_model": "sample_data/spm/tokenizer.model",
+            "spm_vocab": "sample_data/spm/tokenizer.vocab"
+        }
+
+        self._tokenizer = SubwordTokenizer(config=SubwordTokenizerConfig(
+            **self._config))
+
+    def test_num_labels(self):
+        self.assertNotIn("<s>", self._tokenizer.labels)
+        self.assertNotIn("</s>", self._tokenizer.labels)
+        self.assertIn("<blank_id>", self._tokenizer.labels)
+        self.assertEqual("<blank_id>", self._tokenizer.labels[0])
+        self.assertIn("<sos>", self._tokenizer.labels)
+        self.assertEqual("<sos>", self._tokenizer.labels[-1])
+
+    @parameterized.expand([
+        ("season with salt and pepper and a little sugar to taste"),
+        ("struggle warfare was the condition of private ownership it was fatal"
+        ),
+        ("i don't believe ann knew any magic or she'd have worked it before"),
+        ("a black drove came up over the hill behind the wedding party")
+    ])
+    def test_encode_as_tensor(self, text):
+        # Unittest of encode and decode
+        glog.info(text)
+        labels = self._tokenizer.encode(text)
+        decoded = self._tokenizer.decode(labels)
+
+        self.assertEqual(text, decoded)
+
+    @parameterized.expand([
+        ("season with salt and pepper and a little sugar to taste"),
+        ("struggle warfare was the condition of private ownership it was fatal"
+        ),
+        ("i don't believe ann knew any magic or she'd have worked it before"),
+        ("a black drove came up over the hill behind the wedding party")
+    ])
+    def test_encoder_as_tokens(self, text):
+        # Unittest of encode_as_tokens and decode_from_tokens
+        glog.info(text)
+        tokens = self._tokenizer.encode_as_tokens(text)
+        decoded = self._tokenizer.decode_from_tokens(tokens)
+
+        self.assertEqual(text, decoded)
+
+
+if __name__ == "__main__":
+    unittest.main()
