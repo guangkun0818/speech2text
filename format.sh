9381acdbc430e5fda2d9c70fe46cd82caf191d04
guangkun0818 <609946862@qq.com>
2024-04-19 23:38:01 +0800

    [training_utils] - update format scripts.

--- format.sh
+++ format.sh
@@ -7,11 +7,15 @@
 # Code format script
 
 # Python, yapf version 0.32.0
-find ./ -path "./runtime" -prune -o -iname "*.py" -print | xargs yapf -i --style google
+find ./ -path "./runtime" -prune -o \
+    -iname "*.py" -print | xargs yapf -i --style google
 
-# C++ runtime clang-format 13.0.0
-find ./runtime/vad_rt/ -iname "*.h" -o -iname "*.cc" -o -iname "*.cpp" | xargs clang-format -style=Google -i
+# C++ runtime clang-format 16.0.0
+find ./runtime/asr_rt/ -iname "*.h" -o \
+    -iname "*.cc" -o -iname "*.cpp" | xargs clang-format -style=Google -i
 
 # CMakes cmake-format version 0.6.13
 find ./runtime/cmake/ -iname "*.cmake" | xargs cmake-format -i
-find ./runtime/ -path "./runtime/3rd_party" -prune -o -iname "CMakeLists.txt" -print | xargs cmake-format -i
+find ./runtime/ -path "./runtime/3rd_party" -prune -o \
+    -path "./runtime/build" -prune -o \
+    -iname "CMakeLists.txt" -print | xargs cmake-format -i
