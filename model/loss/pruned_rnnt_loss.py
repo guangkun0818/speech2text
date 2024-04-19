57cbb08afc559c9db31f51982e40d68a3beea1a3
guangkun0818 <609946862@qq.com>
2024-04-20 00:25:15 +0800

    [training_utils] - modify pruned_rnnt_loss support k2.

--- model/loss/pruned_rnnt_loss.py
+++ model/loss/pruned_rnnt_loss.py
@@ -8,13 +8,15 @@
 import dataclasses
 import torch
 import torch.nn as nn
-import fast_rnnt
+import k2
 
 
 @dataclasses.dataclass
 class PrunedRnntLossConfig:
     """ Pruned Rnnt Loss Config """
     termination_symbol: int = 0  # <blank id> = 0
+    rnnt_type: str = "regular"
+    delay_penalty: float = 0.0
     reduction: str = "mean"
 
 
@@ -25,18 +27,24 @@ class PrunedRnntLoss(nn.Module):
         super(PrunedRnntLoss, self).__init__()
 
         self._termination_symbol = config.termination_symbol
+        self._rnnt_type = config.rnnt_type
+        self._delay_penalty = config.delay_penalty
         self._reduction = config.reduction
 
     def forward(self, logits: torch.Tensor, targets: torch.Tensor,
                 logit_lengths: torch.Tensor, target_lengths: torch.Tensor,
                 boundary: torch.Tensor, ranges: torch.Tensor):
+
         # NOTE: logits should strictly be fp32 even if 16-mixed specified.
-        loss = fast_rnnt.rnnt_loss_pruned(
+        loss = k2.rnnt_loss_pruned(
             logits=logits.to(torch.float32),
             symbols=targets,
             ranges=ranges,
             termination_symbol=self._termination_symbol,
             boundary=boundary,
+            rnnt_type=self._rnnt_type,
+            delay_penalty=self._delay_penalty,
             reduction=self._reduction,
         )
+
         return loss
