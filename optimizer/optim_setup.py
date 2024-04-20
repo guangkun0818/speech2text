1ce1539d2a72a37178ed176c23d096a34c91f4cd
guangkun0818 <609946862@qq.com>
2024-04-21 00:36:02 +0800

    [training_utils] - impl warmup hold policy scheduler.

--- optimizer/optim_setup.py
+++ optimizer/optim_setup.py
@@ -98,3 +98,144 @@ class WarmupLR(_LRScheduler):
 
     def set_step(self, step: int):
         self.last_epoch = step
+
+
+class WarmupPolicy(_LRScheduler):
+    """ Adds warmup kwargs and warmup logic to lr policy.
+        All arguments should be passed as kwargs for clarity, 
+        
+        Args:
+            warmup_steps: Number of training steps in warmup stage.
+            warmup_ratio: Ratio of warmup steps to total steps.
+            max_steps: Total number of steps while training or `None` for
+                infinite training.
+    """
+
+    def __init__(self,
+                 optimizer,
+                 *,
+                 warmup_steps=None,
+                 warmup_ratio=None,
+                 max_steps=None,
+                 min_lr=0.0,
+                 last_epoch=-1):
+        assert not (warmup_steps is not None and warmup_ratio is not None
+                   ), "Either use particular number of step or ratio"
+        assert warmup_ratio is None or max_steps is not None, "If there is a ratio, there should be a total steps"
+
+        # It is necessary to assign all attributes *before* __init__,
+        # as class is wrapped by an inner class.
+        self.max_steps = max_steps
+        if warmup_steps is not None:
+            self.warmup_steps = warmup_steps
+        elif warmup_ratio is not None:
+            self.warmup_steps = int(warmup_ratio * max_steps)
+        else:
+            self.warmup_steps = 0
+
+        self.min_lr = min_lr
+        super().__init__(optimizer, last_epoch)
+
+    def get_lr(self):
+        if not self._get_lr_called_within_step:
+            warnings.warn(
+                "To get the last learning rate computed by the scheduler, please use `get_last_lr()`.",
+                UserWarning)
+
+        step = self.last_epoch
+
+        if step < self.warmup_steps and self.warmup_steps > 0:
+            return self._get_warmup_lr(step)
+
+        if (self.max_steps is not None) and (step > self.max_steps):
+            return [self.min_lr for _ in self.base_lrs]
+
+        return self._get_lr(step)
+
+    def _get_warmup_lr(self, step):
+        lr_val = (step + 1) / (self.warmup_steps + 1)
+        return [initial_lr * lr_val for initial_lr in self.base_lrs]
+
+    def _get_lr(self, step):
+        """ Simple const lr policy """
+        return self.base_lrs
+
+
+class WarmupHoldPolicy(WarmupPolicy):
+    """ Variant of WarmupPolicy which maintains high learning rate for a 
+        defined number of steps. All arguments should be passed as kwargs 
+        for clarity. 
+        
+        Args:
+            warmup_steps: Number of training steps in warmup stage.
+            warmup_ratio: Ratio of warmup steps to total steps.
+            hold_steps: Number of training steps to hold the learning rate after 
+                warm up.
+            hold_ratio: Ratio of hold steps to total steps.
+            max_steps: Total number of steps while training or 'None for
+                infinite training
+    """
+
+    def __init__(self,
+                 optimizer,
+                 *,
+                 warmup_steps=None,
+                 warmup_ratio=None,
+                 hold_steps=None,
+                 hold_ratio=None,
+                 max_steps=None,
+                 min_lr=0.0,
+                 last_epoch=-1):
+
+        assert not (hold_steps is not None and hold_ratio is not None
+                   ), "Either use particular number of step or ratio"
+        assert hold_ratio is None or max_steps is not None, "If there is a ratio, there should be a total steps"
+
+        self.min_lr = min_lr
+        self._last_warmup_1r = 0.0
+
+        # Necessary to duplicate as class attributes are hidden in inner class
+        self.max_steps = max_steps
+        if warmup_steps is not None:
+            self.warmup_steps = warmup_steps
+        elif warmup_ratio is not None:
+            self.warmup_steps = int(warmup_ratio * max_steps)
+        else:
+            self.warmup_steps = 0
+
+        if hold_steps is not None:
+            self.hold_steps = hold_steps + self.warmup_steps
+        elif hold_ratio is not None:
+            self.hold_steps = int(hold_ratio * max_steps) + self.warmup_steps
+        else:
+            self.hold_steps = 0
+
+        super().__init__(
+            optimizer,
+            warmup_steps=warmup_steps,
+            warmup_ratio=warmup_ratio,
+            max_steps=max_steps,
+            last_epoch=last_epoch,
+            min_lr=min_lr,
+        )
+
+    def get_lr(self):
+        if not self._get_lr_called_within_step:
+            warnings.warn(
+                "To get the last learning rate computed by the scheduler, "
+                "please use `get_last_lr()`.", UserWarning)
+
+        step = self.last_epoch
+
+        # Warmup phase
+        if step <= self.warmup_steps and self.warmup_steps > 0:
+            return self._get_warmup_lr(step)
+
+        # Hold phase
+        if (step >= self.warmup_steps) and (step < self.hold_steps):
+            return self.base_lrs
+
+        if step > self.max_steps:
+            return [self.min_lr for _ in self.base_lrs]
+
+        return self._get_lr(step)
