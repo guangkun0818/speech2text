143c3810fb0b247692ab228e5eb856278789557f
guangkun0818 <609946862@qq.com>
2024-08-26 02:33:14 +0800

    [zipformer_impl] - setting up zipformer impl.

---
+++ model/encoder/zipformer.py
@@ -0,0 +1,6 @@
+# Author: guangkun0818
+# Email: 609946862@qq.com
+# Created on 2024.08.26
+""" Zipformer impl, based on https://arxiv.org/pdf/2310.11230v4. Modified from 
+    https://github.com/k2-fsa/icefall/blob/master/egs/librispeech/ASR/zipformer/zipformer.py 
+"""
---
+++ model/encoder/zipformer_test.py
@@ -0,0 +1,4 @@
+# Author: guangkun0818
+# Email: 609946862@qq.com
+# Created on 2024.08.26
+""" Unittest of Zipformer impl. """
